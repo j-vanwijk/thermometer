b8c1e9dd52cba14e2f43ba5f25e4c9e848c83cca
j-vanwijk <60421659+j-vanwijk@users.noreply.github.com>
2021-11-13 13:49:46 +0100

    deel 3

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
+let temperatuur = 0
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(input.temperature() - 8)
+    if (temperatuur > 21) {
+        basic.showString("Warm korte broek")
+    } else if (temperatuur > 15) {
+        basic.showString("Lekker weer! T-shirt")
+    } else if (temperatuur < 15) {
+        basic.showString("Brrr! Warme trui!")
+    }
 })
 basic.forever(function () {
-	
+    temperatuur = input.temperature() - 8
 })
